3fde8985b340313c9db15482fecce3298021fe9c
Ryan Seys <ryan@ryanseys.com>
2014-03-09 13:23:41 -0400

    Add - / + precedence removing need for spaces

--- gram.y
+++ gram.y
@@ -12,43 +12,37 @@ int yylex();
   double aDouble;
 }
 
+%left '+' '-'
+%left UMINUS
+
 %%
-S :
-  { $$ = 0; }
-  | S E NEWLINE
-  { printf("%f\n", $2 ); $$ = 0; }
+S : { $$ = 0; }
+  | S E NEWLINE { printf("%f\n", $2 ); $$ = 0; }
   ;
 
-E : E '+' T
-  { $$ = $1 + $3; }
-  | E '-' T
-  { $$ = $1 - $3; }
-  | T
-  { $$ = $1; }
+E : E '+' T { $$ = $1 + $3; }
+  | E '-' T { $$ = $1 - $3; }
+  | T { $$ = $1; }
   ;
 
-T : T '*' P
-  { $$ = $1 * $3; }
-  | T '/' P
-  { $$ = $1 / $3; }
-  | P
-  { $$ = $1; }
+T : T '*' P { $$ = $1 * $3; }
+  | T '/' P { $$ = $1 / $3; }
+  | P { $$ = $1; }
   ;
 
-P : F '^' P
-  { $$ = pow($1, $3); }
-  | F
-  { $$ = $1; }
+P : F '^' P { $$ = pow($1, $3); }
+  | F { $$ = $1; }
   ;
 
-F : '(' E ')'
-  { ($$ = $2) }
-  | DOUBLE
-  { $$ = $1; }
+F : '(' E ')' { $$ = $2; }
+  | DOUBLE { $$ = $1; }
+  | '+' E { $$ = $2; } %prec UMINUS
+  | '-' E { $$ = - $2; } %prec UMINUS
   ;
+
 
 %%
 int yyerror( char * s ) {
-  fprintf( stderr, "%s\n", s );
-  return 0;
+  fprintf( stderr, "ERROR: %s\n", s );
+  return -1;
 }
--- tests.sh
+++ tests.sh
@@ -2,9 +2,6 @@
 OUT1=$(echo '(1 * 4) ^ (4 / 2) * 5' | ./add.out)
 RES1='80.000000'
 
-OUT2=$(echo '1 / 4' | ./add.out)
-RES2='0.250000'
-
 if [ "$OUT1" == "$RES1" ]
 then
   echo "Test passed! Result: $OUT1"
@@ -14,6 +11,9 @@ else
   echo "Actual:   $OUT1"
 fi
 
+OUT2=$(echo '1 / 4' | ./add.out)
+RES2='0.250000'
+
 if [ "$OUT2" == "$RES2" ]
 then
   echo "Test passed! Result: $OUT2"
@@ -21,4 +21,52 @@ else
   echo "Test failed!"
   echo "Expected: $RES2"
   echo "Actual:   $OUT2"
+fi
+
+OUT3=$(echo '1 + 4' | ./add.out)
+RES3='5.000000'
+
+if [ "$OUT3" == "$RES3" ]
+then
+  echo "Test passed! Result: $OUT3"
+else
+  echo "Test failed!"
+  echo "Expected: $RES3"
+  echo "Actual:   $OUT3"
+fi
+
+OUT4=$(echo '-1 + -4' | ./add.out)
+RES4='-5.000000'
+
+if [ "$OUT4" == "$RES4" ]
+then
+  echo "Test passed! Result: $OUT4"
+else
+  echo "Test failed!"
+  echo "Expected: $RES4"
+  echo "Actual:   $OUT4"
+fi
+
+OUT5=$(echo '-1--4' | ./add.out)
+RES5='3.000000'
+
+if [ "$OUT5" == "$RES5" ]
+then
+  echo "Test passed! Result: $OUT5"
+else
+  echo "Test failed!"
+  echo "Expected: $RES5"
+  echo "Actual:   $OUT5"
+fi
+
+OUT6=$(echo '4^-1' | ./add.out)
+RES6='0.250000'
+
+if [ "$OUT6" == "$RES6" ]
+then
+  echo "Test passed! Result: $OUT6"
+else
+  echo "Test failed!"
+  echo "Expected: $RES6"
+  echo "Actual:   $OUT6"
 fi
