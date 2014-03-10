b4ed16a942c78d0637925b6a39152a2abe2a9520
Ryan Seys <ryan@ryanseys.com>
2014-03-10 16:21:14 -0400

    Clean up test cases

--- tests.sh
+++ tests.sh
@@ -1,156 +1,40 @@
-#!/bin/sh
-OUT1=$(echo '(1 * 4) ^ (4 / 2) * 5' | ./add.out)
-RES1='80.000000'
+#!/usr/bin/env bash
 
-if [ "$OUT1" == "$RES1" ]
-then
-  echo "Test passed! Result: $OUT1"
-else
-  echo "Test failed!"
-  echo "Expected: $RES1"
-  echo "Actual:   $OUT1"
-fi
+out[1]=$(echo '(1 * 4) ^ (4 / 2) * 5' | ./add.out)
+res[1]='80.000000'
+out[2]=$(echo '1 / 4' | ./add.out)
+res[2]='0.250000'
+out[3]=$(echo '1 + 4' | ./add.out)
+res[3]='5.000000'
+out[4]=$(echo '-1 + -4' | ./add.out)
+res[4]='-5.000000'
+out[5]=$(echo '-1--4' | ./add.out)
+res[5]='3.000000'
+out[6]=$(echo '4^-1' | ./add.out)
+res[6]='0.250000'
+out[7]=$(echo '(1+1)*4^log((6^2)^(1/2))' | ./add.out)
+res[7]='23.975998'
+out[8]=$(echo '(1+1)*4^log((6^2)^1/2)' | ./add.out)
+res[8]='109.953027'
+out[8]=$(echo '2^1/2' | ./add.out)
+res[8]='1.000000'
+out[9]=$(echo '2^1+1' | ./add.out)
+res[9]='3.000000'
+out[10]=$(echo '1^2^3^4' | ./add.out)
+res[10]='1.000000'
+out[11]=$(echo '2^3^2' | ./add.out)
+res[11]='512.000000'
+out[12]=$(echo 'exp(4)' | ./add.out)
+res[12]='54.598150'
 
-OUT2=$(echo '1 / 4' | ./add.out)
-RES2='0.250000'
-
-if [ "$OUT2" == "$RES2" ]
-then
-  echo "Test passed! Result: $OUT2"
-else
-  echo "Test failed!"
-  echo "Expected: $RES2"
-  echo "Actual:   $OUT2"
-fi
-
-OUT3=$(echo '1 + 4' | ./add.out)
-RES3='5.000000'
-
-if [ "$OUT3" == "$RES3" ]
-then
-  echo "Test passed! Result: $OUT3"
-else
-  echo "Test failed!"
-  echo "Expected: $RES3"
-  echo "Actual:   $OUT3"
-fi
-
-OUT4=$(echo '-1 + -4' | ./add.out)
-RES4='-5.000000'
-
-if [ "$OUT4" == "$RES4" ]
-then
-  echo "Test passed! Result: $OUT4"
-else
-  echo "Test failed!"
-  echo "Expected: $RES4"
-  echo "Actual:   $OUT4"
-fi
-
-OUT5=$(echo '-1--4' | ./add.out)
-RES5='3.000000'
-
-if [ "$OUT5" == "$RES5" ]
-then
-  echo "Test passed! Result: $OUT5"
-else
-  echo "Test failed!"
-  echo "Expected: $RES5"
-  echo "Actual:   $OUT5"
-fi
-
-OUT6=$(echo '4^-1' | ./add.out)
-RES6='0.250000'
-
-if [ "$OUT6" == "$RES6" ]
-then
-  echo "Test passed! Result: $OUT6"
-else
-  echo "Test failed!"
-  echo "Expected: $RES6"
-  echo "Actual:   $OUT6"
-fi
-
-OUT7=$(echo '(1+1)*4^log((6^2)^(1/2))' | ./add.out)
-RES7='23.975998'
-
-if [ "$OUT7" == "$RES7" ]
-then
-  echo "Test passed! Result: $OUT7"
-else
-  echo "Test failed!"
-  echo "Expected: $RES7"
-  echo "Actual:   $OUT7"
-fi
-
-OUT8=$(echo '(1+1)*4^log((6^2)^1/2)' | ./add.out)
-RES8='109.953027'
-
-if [ "$OUT8" == "$RES8" ]
-then
-  echo "Test passed! Result: $OUT8"
-else
-  echo "Test failed!"
-  echo "Expected: $RES8"
-  echo "Actual:   $OUT8"
-fi
-
-OUT8=$(echo '2^1/2' | ./add.out)
-RES8='1.000000'
-
-if [ "$OUT8" == "$RES8" ]
-then
-  echo "Test passed! Result: $OUT8"
-else
-  echo "Test failed!"
-  echo "Expected: $RES8"
-  echo "Actual:   $OUT8"
-fi
-
-OUT9=$(echo '2^1+1' | ./add.out)
-RES9='3.000000'
-
-if [ "$OUT9" == "$RES9" ]
-then
-  echo "Test passed! Result: $OUT9"
-else
-  echo "Test failed!"
-  echo "Expected: $RES9"
-  echo "Actual:   $OUT9"
-fi
-
-OUT10=$(echo '1^2^3^4' | ./add.out)
-RES10='1.000000'
-
-if [ "$OUT10" == "$RES10" ]
-then
-  echo "Test passed! Result: $OUT10"
-else
-  echo "Test failed!"
-  echo "Expected: $RES10"
-  echo "Actual:   $OUT10"
-fi
-
-OUT10=$(echo '2^3^2' | ./add.out)
-RES10='512.000000'
-
-if [ "$OUT10" == "$RES10" ]
-then
-  echo "Test passed! Result: $OUT10"
-else
-  echo "Test failed!"
-  echo "Expected: $RES10"
-  echo "Actual:   $OUT10"
-fi
-
-OUT11=$(echo 'exp(4)' | ./add.out)
-RES11='54.598150'
-
-if [ "$OUT11" == "$RES11" ]
-then
-  echo "Test passed! Result: $OUT11"
-else
-  echo "Test failed!"
-  echo "Expected: $RES11"
-  echo "Actual:   $OUT11"
-fi
+for i in {1..12}
+do
+  if [ "${out[$i]}" == "${res[$i]}" ]
+  then
+    echo "Test passed! Result: ${out[$i]}"
+  else
+    echo "Test failed!"
+    echo "Expected: ${res[$i]}"
+    echo "Actual:   ${out[$i]}"
+  fi
+done
